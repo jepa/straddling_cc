16363ca3e729fc9c6f8ca27dafa09e28cf9bff2d
Juliano Palacios Abrantes <j.palacios@oceans.ubc.ca>
2023-12-22 22:26:50 -0300

    finalized circle figures for realms and rfmos

--- functions/circle_figures_fx.R
+++ functions/circle_figures_fx.R
@@ -18,8 +18,13 @@ circular_plot <- function(data,period_val,rcp_val,change_val,level = "realm"){
     
     name <- paste0("./results/figures/circle/",level,"_circular_",change_val,"_",period_val,"_",rcp_val,".png")
     
-    png(name,
-        width = 1300, height = 1300, res = 150, units = "px")
+    if(level == "realm"){
+      png(name,
+          width = 1300, height = 1300, res = 150, units = "px")
+    }else(
+      png(name,
+          width = 700, height = 700, res = 150, units = "px")
+    )
     chordDiagram(
       x = circle_data %>% arrange(rowname), 
       grid.col = mycolor_scale,
@@ -39,39 +44,61 @@ circular_plot <- function(data,period_val,rcp_val,change_val,level = "realm"){
     )
     dev.off()
     
+    
+    # Same plots
+    
     if(change_val == "same"){
       name <- paste0("./results/figures/circle/",level,"_circular_",change_val,"_",period_val,"_",rcp_val,".png")
-      png(name,
-          width = 1300, height = 1300, res = 150, units = "px")
-      chordDiagram(
-        x = circle_data %>% arrange(rowname), 
-        grid.col = mycolor_scale,
-        directional = ifelse(change_val == "gain",1, -1),
-        direction.type = ifelse(change_val == "same","diffHeight","arrows"),
-        link.arr.type = "big.arrow",
-        link.arr.length = 0.1,
-        link.sort = T,
-        link.auto = T,
-        link.largest.ontop = TRUE,
-        transparency = 0.30,
-        scale = T,
-        symmetric = T,
-        big.gap = 5,
-        if(level == "realm"){
-        c(annotationTrack = "grid",
-        preAllocateTracks = list(track.height = max(strwidth(unlist(dimnames(circle_data))))))
-        }
-      )
+      
       if(level == "realm"){
-      # we go back to the first track and customize sector labels
-      circos.track(track.index = 1, panel.fun = function(x, y) {
-        circos.text(CELL_META$xcenter, CELL_META$ylim[1], CELL_META$sector.index, 
-                    facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5))
-      }, bg.border = NA) # here set bg.border to NA is important
+        png(name,
+            width = 1300, height = 1300, res = 150, units = "px")
+        chordDiagram(
+          x = circle_data %>% arrange(rowname), 
+          grid.col = mycolor_scale,
+          directional = ifelse(change_val == "gain",1, -1),
+          direction.type = ifelse(change_val == "same","diffHeight","arrows"),
+          link.arr.type = "big.arrow",
+          link.arr.length = 0.1,
+          link.sort = T,
+          link.auto = T,
+          link.largest.ontop = TRUE,
+          transparency = 0.30,
+          scale = T,
+          symmetric = T,
+          big.gap = 5,
+          annotationTrack = "grid",
+          preAllocateTracks = list(track.height = max(strwidth(unlist(dimnames(circle_data)))))
+        )
+        
+        # we go back to the first track and customize sector labels
+        circos.track(track.index = 1, panel.fun = function(x, y) {
+          circos.text(CELL_META$xcenter, CELL_META$ylim[1], CELL_META$sector.index, 
+                      facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5))
+        }, bg.border = NA)
+        # here set bg.border to NA is important
+      }else{
+        png(name,
+            width = 700, height = 700, res = 150, units = "px")
+        chordDiagram(
+          x = circle_data %>% arrange(rowname), 
+          grid.col = mycolor_scale,
+          directional = ifelse(change_val == "gain",1, -1),
+          direction.type = ifelse(change_val == "same","diffHeight","arrows"),
+          link.arr.type = "big.arrow",
+          link.arr.length = 0.1,
+          link.sort = T,
+          link.auto = T,
+          link.largest.ontop = TRUE,
+          transparency = 0.30,
+          scale = T,
+          symmetric = T,
+          big.gap = 5
+        )
       }
-    
-    dev.off()
-    
+      
+      dev.off()
+      
     }
     
   }else{
@@ -86,9 +113,9 @@ circular_plot <- function(data,period_val,rcp_val,change_val,level = "realm"){
                 .funs = replace_na,0
       ) %>% 
       mutate(
-             value = gain - lost,
-             rowname_b = ifelse(value > 0,name,rfmo_name),
-             key_b = ifelse(value < 0,name,rfmo_name)) %>% 
+        value = gain - lost,
+        rowname_b = ifelse(value > 0,name,rfmo_name),
+        key_b = ifelse(value < 0,name,rfmo_name)) %>% 
       # View()
       ungroup() %>%
       mutate(
@@ -101,45 +128,70 @@ circular_plot <- function(data,period_val,rcp_val,change_val,level = "realm"){
              rcp == rcp_val) %>% 
       select(rowname = rowname_b,
              key = key_b,
-             value) %>% 
-      filter(value != 0)
+             value)
+    
+    if(level =="realm"){
+      net_change <- net_change %>% 
+        filter(value != 0)
+    }else{
+      net_change <- net_change %>% 
+        filter(value > 0)
+    }
+    
     
     
     
     name_net <- paste0("./results/figures/circle/",level,"_circular_net_",period_val,"_",rcp_val,".png")
-    png(name_net,
-        width = 1300, height = 1300, res = 150, units = "px")
     
-    chordDiagram(
-      x = net_change %>% mutate(key = ifelse(key == "Temperate\nN. Pacific", "T.N. Pacific",
-                                             ifelse(key == "Temperate\nN. Atlantic", "T.N. Atlantic",key))), 
-      grid.col = mycolor_scale,
-      directional = 1, 
-      direction.type = c("diffHeight","arrows"),
-      link.arr.type = "big.arrow",
-      link.arr.length = 0.1,
-      link.sort = F,
-      link.largest.ontop = TRUE,
-      transparency = 0.30,
-      scale = F,
-      symmetric = F,
-      if(level == "realm"){
-      c(annotationTrack = "grid", 
-      preAllocateTracks = list(track.height = max(strwidth(unlist(dimnames(net_change))))))
-      }
-    )
-    if(level == "realm"){
-    circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
-      xlim = get.cell.meta.data("xlim")
-      ylim = get.cell.meta.data("ylim")
-      sector.name = get.cell.meta.data("sector.index")
-      circos.text(mean(xlim), ylim[1] + .1, sector.name, facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5))
-      circos.axis(h = "top", labels.cex = 0.5, major.tick.length = 0.2, sector.index = sector.name, track.index = 2)
-    }, bg.border = NA)
+    
+    if(level =="realm"){
+      png(name_net,
+          width = 1300, height = 1300, res = 150, units = "px")
+      chordDiagram(
+        x = net_change %>% mutate(key = ifelse(key == "Temperate\nN. Pacific", "T.N. Pacific",
+                                               ifelse(key == "Temperate\nN. Atlantic", "T.N. Atlantic",key))), 
+        grid.col = mycolor_scale,
+        directional = 1, 
+        direction.type = c("diffHeight","arrows"),
+        link.arr.type = "big.arrow",
+        link.arr.length = 0.1,
+        link.sort = F,
+        link.largest.ontop = TRUE,
+        transparency = 0.30,
+        scale = F,
+        symmetric = F,
+        annotationTrack = "grid", 
+        preAllocateTracks = list(track.height = max(strwidth(unlist(dimnames(net_change)))))
+      )
+      
+      circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
+        xlim = get.cell.meta.data("xlim")
+        ylim = get.cell.meta.data("ylim")
+        sector.name = get.cell.meta.data("sector.index")
+        circos.text(mean(xlim), ylim[1] + .1, sector.name, facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5))
+        circos.axis(h = "top", labels.cex = 0.5, major.tick.length = 0.2, sector.index = sector.name, track.index = 2)
+      }, bg.border = NA)
+      
+    }else{
+      png(name_net,
+          width = 700, height = 700, res = 150, units = "px")
+      chordDiagram(
+        x = net_change %>% mutate(key = ifelse(key == "Temperate\nN. Pacific", "T.N. Pacific",
+                                               ifelse(key == "Temperate\nN. Atlantic", "T.N. Atlantic",key))), 
+        grid.col = mycolor_scale,
+        directional = 1, 
+        direction.type = c("diffHeight","arrows"),
+        link.arr.type = "big.arrow",
+        link.arr.length = 0.1,
+        link.sort = F,
+        link.largest.ontop = TRUE,
+        transparency = 0.30,
+        scale = F,
+        symmetric = F
+      )
     }
-    dev.off() 
     
+    dev.off() 
   }
-  
 }
 
